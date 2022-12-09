a0c49a43f9e8c36cd15808f4ce326715fc187613
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-09 08:47:56 +0100

    QR kód generálása valamilyen értékkel

--- app/src/main/java/hu/petrik/qrcode/MainActivity.java
+++ app/src/main/java/hu/petrik/qrcode/MainActivity.java
@@ -2,11 +2,20 @@ package hu.petrik.qrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextQRData;
@@ -20,7 +29,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        buttonGenerate.setOnClickListener(view -> {
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            String data = editTextQRData.getText().toString().trim();
+            if (data.isEmpty()) {
+                Toast.makeText(this, "Kötelező adatot megadni", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(data, BarcodeFormat.QR_CODE, 250, 250);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    imageViewQRResult.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
 
     private void init() {
         editTextQRData = findViewById(R.id.editTextQRData);

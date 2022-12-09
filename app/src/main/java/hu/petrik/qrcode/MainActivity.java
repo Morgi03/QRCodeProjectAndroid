d454c777678e07e6cf60ae46385802ad5a610046
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-09 08:28:18 +0100

    inicializálás

--- app/src/main/java/hu/petrik/qrcode/MainActivity.java
+++ app/src/main/java/hu/petrik/qrcode/MainActivity.java
@@ -3,12 +3,30 @@ package hu.petrik.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextQRData;
+    private Button buttonGenerate;
+    private Button buttonScan;
+    private ImageView imageViewQRResult;
+    private TextView textViewQRResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        editTextQRData = findViewById(R.id.editTextQRData);
+        buttonGenerate = findViewById(R.id.buttonGenerate);
+        buttonScan = findViewById(R.id.buttonScan);
+        imageViewQRResult = findViewById(R.id.imageViewQRResult);
+        textViewQRResult = findViewById(R.id.textViewQRResult);
     }
 }
